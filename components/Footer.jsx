99a41c4a630e33ed7edfe272f5e308fd85b82217
Harshit <harshitbhardwaj001@gmail.com>
2024-04-02 03:53:02 +0530

    Footer Done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="w-[100vw] h-[70vh] flex gap-[8rem] bg-[#101821] px-[5rem] items-center">
+      <div className="flex flex-col gap-3 w-[35vw]">
+        <h1 className="text-[35px] font-semibold text-white">ABOUT PAPEL</h1>
+        <p className="text-[18px] text-[#d6d6d6]">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut pariatur
+          porro a ad quasi veniam aliquid quaerat. Reiciendis vel accusamus
+          beatae eum ea, dignissimos pariatur ipsa, veritatis ratione iusto
+          maiores. Minus numquam incidunt corporis recusandae enim, iste aliquid
+          possimus asperiores molestiae, molestias qui maxime quisquam! maiores.
+          Minus numquam incidunt corporis recusandae enim, iste aliquid possimus
+          asperiores molestiae, molestias qui maxime quisquam!
+        </p>
+        <Image src="/wave-design-2.png" alt="wave" width={400} height={200} />
+      </div>
+      <div className="flex flex-col gap-4 text-[28px] font-medium text-white w-[20vw]">
+        <h1>PAPEL FAQs</h1>
+        <h1>INVESTORS</h1>
+        <h1>PRIVACY POLICY</h1>
+        <h1>TERMS OF USE</h1>
+        <h1>CONTACT US</h1>
+      </div>
+      <div className="flex flex-col gap-[0.7rem]">
+        <h1 className="font-medium text-[28px] text-white">Payment</h1>
+        <h1 className="font-medium text-[22px] text-[#bebebe] leading-[1.5rem]">
+          Secured payments <br /> powered by
+        </h1>
+        <div className="w-[17vw] h-[21vh] bg-[#d6d6d6] mt-[0.8rem]"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- components/Home/About.jsx
+++ components/Home/About.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="w-[100vw] h-[130vh] bg-[#97D4E3] flex gap-[8rem] px-[8rem] pt-[12rem] justify-center">
+      <div className="w-[35vw] h-[95vh] rounded-[3rem] shadow-lg bg-[#fff]"></div>
+      <div className="flex flex-col gap-[1rem]">
+        <h1 className="text-[70px] font-extrabold leading-[5rem]">ABOUT US</h1>
+        <p className="text-[22px] text-[#1B3C87]">
+          You say you love the planet. <br /> And if you truly do, <br /> you
+          must've felt that guilt while grabbing a plastic <br /> water bottle
+          in your hands... <br /> We know the feeling... <br />
+          And that's why, we, at PAPEL believe that our <br />
+          hydration shouldn'thurt our home. <br /> Grab your eco-friendly
+          hydration products to give a <br /> loving hug to nature.
+        </p>
+        <p className="text-[25px] text-[#1B3C87]">
+          With <strong className="text-[32px] italic">PAPEL</strong>, here's
+          your chance to
+        </p>
+        <h1 className="text-[35px] text-[#1B3C87] font-bold italic">
+          Drink The Change
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
