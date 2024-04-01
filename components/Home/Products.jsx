bde4a257a62890f0ddafef831ec83c9af65b0b24
Harshit <harshitbhardwaj001@gmail.com>
2024-04-02 03:27:57 +0530

    Slogan Added

--- components/Home/Products.jsx
+++ components/Home/Products.jsx
@@ -1,43 +1,110 @@
+import Image from "next/image";
 import React from "react";
 
 const Products = () => {
   return (
-    <div className="w-[100vw] h-[100vh] flex flex-col pt-[3rem] items-center gap-10">
-      <div className="absolute top-[270%] bg-[#97D4E3] h-[50vh] w-[100vw] z-[-1]"></div>
+    <div className="w-[100vw] h-[110vh] flex flex-col pt-[3rem] items-center gap-10">
+      <div className="absolute top-[270%] bg-[#97D4E3] h-[60vh] w-[100vw] z-[-1]"></div>
       <h1 className="text-[60px] font-extrabold">PRODUCTS</h1>
       <p className="text-[22px] text-[#1B3C87] font-medium mt-[-3rem] tracking-wide">
         Here you can see all our products
       </p>
       <div className="flex gap-12">
         <div className="video-card">
-          <iframe
-            width="350"
-            height="420"
-            src="https://www.youtube.com/embed/9Q9xJ3Hl1wA"
-            className="rounded-[3rem] shadow-xl"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          ></iframe>
+          <div className="flex flex-col gap-5">
+            <div className="w-[350px] h-[360px] bg-[#fff] shadow-xl rounded-[3rem]">
+              <Image
+                src="/papel-product-png.png"
+                alt="product-1"
+                width={400}
+                height={400}
+                className="pt-[1rem]"
+              />
+            </div>
+            <div className="flex gap-10 px-[0.8rem]">
+              <div className="flex flex-col text-[20px]">
+                <h1>Product Name</h1>
+                <h1>Quantity 6 Packs</h1>
+                <h1>Cost &#8377;300</h1>
+              </div>
+              <div className="flex flex-col justify-center">
+                <h1>Add to cart</h1>
+                <div className="flex gap-4">
+                  <div className="w-[30px] h-[30px] bg-[#fff] shadow-lg rounded-md text-center text-[22px]">
+                    -
+                  </div>
+                  <div className="text-center text-[22px]">1</div>
+                  <div className="w-[30px] h-[30px] bg-[#fff] shadow-lg rounded-md text-center text-[22px]">
+                    +
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="video-card">
-          <iframe
-            width="350"
-            height="420"
-            src="https://www.youtube.com/embed/9Q9xJ3Hl1wA"
-            className="rounded-[3rem] shadow-xl"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          ></iframe>
+          <div className="flex flex-col gap-5">
+            <div className="w-[350px] h-[360px] bg-[#fff] shadow-xl rounded-[3rem]">
+              <Image
+                src="/papel-product-png.png"
+                alt="product-1"
+                width={400}
+                height={400}
+                className="pt-[1rem]"
+              />
+            </div>
+            <div className="flex gap-10 px-[0.8rem]">
+              <div className="flex flex-col text-[20px]">
+                <h1>Product Name</h1>
+                <h1>Quantity 12 Packs</h1>
+                <h1>Cost &#8377;300</h1>
+              </div>
+              <div className="flex flex-col justify-center">
+                <h1>Add to cart</h1>
+                <div className="flex gap-4">
+                  <div className="w-[30px] h-[30px] bg-[#fff] shadow-lg rounded-md text-center text-[22px]">
+                    -
+                  </div>
+                  <div className="text-center text-[22px]">1</div>
+                  <div className="w-[30px] h-[30px] bg-[#fff] shadow-lg rounded-md text-center text-[22px]">
+                    +
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="video-card">
-          <iframe
-            width="350"
-            height="420"
-            src="https://www.youtube.com/embed/9Q9xJ3Hl1wA"
-            className="rounded-[3rem] shadow-xl"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          ></iframe>
+          <div className="flex flex-col gap-5">
+            <div className="w-[350px] h-[360px] bg-[#fff] shadow-xl rounded-[3rem]">
+              <Image
+                src="/papel-product-png.png"
+                alt="product-1"
+                width={400}
+                height={400}
+                className="pt-[1rem]"
+              />
+            </div>
+            <div className="flex gap-10 px-[0.8rem]">
+              <div className="flex flex-col text-[20px]">
+                <h1>Product Name</h1>
+                <h1>Quantity 24 Packs</h1>
+                <h1>Cost &#8377;300</h1>
+              </div>
+              <div className="flex flex-col justify-center">
+                <h1>Add to cart</h1>
+                <div className="flex gap-4">
+                  <div className="w-[30px] h-[30px] bg-[#fff] shadow-lg rounded-md text-center text-[22px]">
+                    -
+                  </div>
+                  <div className="text-center text-[22px]">1</div>
+                  <div className="w-[30px] h-[30px] bg-[#fff] shadow-lg rounded-md text-center text-[22px]">
+                    +
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- components/Home/Slogan.jsx
+++ components/Home/Slogan.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Slogan = () => {
   return (
-    <div>Slogan</div>
-  )
-}
+    <div className="w-[100vw] h-[120vh] bg-[#97D4E3] flex gap-[8rem] px-[8rem] pt-[7rem] justify-center">
+      <div className="flex flex-col gap-[1rem]">
+        <h1 className="text-[70px] font-extrabold leading-[5rem]">
+          DRINK <br />
+          THE <br /> CHANGE
+        </h1>
+        <p className="text-[22px] text-[#1B3C87]">
+          You say you love the planet. <br /> And if you truly do, <br /> you
+          must've felt that guilt while grabbing a plastic <br /> water bottle
+          in your hands... <br /> We know the feeling... <br />
+          And that's why, we, at PAPEL believe that our <br />
+          hydration shouldn'thurt our home. <br /> Grab your eco-friendly
+          hydration products to give a <br /> loving hug to nature.
+        </p>
+        <p className="text-[25px] text-[#1B3C87]">
+          With <strong className="text-[32px] italic">PAPEL</strong>, here's
+          your chance to
+        </p>
+        <h1 className="text-[35px] text-[#1B3C87] font-bold italic">
+          Drink The Change
+        </h1>
+      </div>
+      <div className="w-[35vw] h-[95vh] rounded-[3rem] shadow-lg bg-[#fff]"></div>
+    </div>
+  );
+};
 
-export default Slogan
+export default Slogan;
