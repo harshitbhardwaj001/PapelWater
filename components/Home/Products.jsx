612a99a7eb3a256320cb81d92f89b00bb1ddf8cf
Harshit <harshitbhardwaj001@gmail.com>
2024-04-02 01:03:43 +0530

    Videos and Product Done

--- components/Home/Products.jsx
+++ components/Home/Products.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <div className="w-[100vw] h-[100vh] flex flex-col pt-[3rem] items-center gap-10">
+      <div className="absolute top-[270%] bg-[#97D4E3] h-[50vh] w-[100vw] z-[-1]"></div>
+      <h1 className="text-[60px] font-extrabold">PRODUCTS</h1>
+      <p className="text-[22px] text-[#1B3C87] font-medium mt-[-3rem] tracking-wide">
+        Here you can see all our products
+      </p>
+      <div className="flex gap-12">
+        <div className="video-card">
+          <iframe
+            width="350"
+            height="420"
+            src="https://www.youtube.com/embed/9Q9xJ3Hl1wA"
+            className="rounded-[3rem] shadow-xl"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        </div>
+        <div className="video-card">
+          <iframe
+            width="350"
+            height="420"
+            src="https://www.youtube.com/embed/9Q9xJ3Hl1wA"
+            className="rounded-[3rem] shadow-xl"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        </div>
+        <div className="video-card">
+          <iframe
+            width="350"
+            height="420"
+            src="https://www.youtube.com/embed/9Q9xJ3Hl1wA"
+            className="rounded-[3rem] shadow-xl"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
--- components/Home/Videos.jsx
+++ components/Home/Videos.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Videos = () => {
   return (
-    <div>Videos</div>
-  )
-}
+    <div className="w-[100vw] h-[120vh] flex flex-col pt-[3rem] items-center gap-10">
+      <h1 className="text-[60px] font-extrabold">VIDEOS</h1>
+      <p className="text-[22px] text-[#1B3C87] font-medium mt-[-3.7rem]">
+        Here you can see small videos we made with influencers
+      </p>
+      <div className="flex gap-12">
+        <div className="video-card">
+          <iframe
+            width="350"
+            height="550"
+            src="https://www.youtube.com/embed/9Q9xJ3Hl1wA"
+            className="rounded-[3rem] shadow-xl"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        </div>
+        <div className="video-card">
+          <iframe
+            width="350"
+            height="550"
+            src="https://www.youtube.com/embed/9Q9xJ3Hl1wA"
+            className="rounded-[3rem] shadow-xl"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        </div>
+        <div className="video-card">
+          <iframe
+            width="350"
+            height="550"
+            src="https://www.youtube.com/embed/9Q9xJ3Hl1wA"
+            className="rounded-[3rem] shadow-xl"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Videos
+export default Videos;
