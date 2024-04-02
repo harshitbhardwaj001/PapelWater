75090293dac5ce25218599b323eb6118746fa77d
Harshit <harshitbhardwaj001@gmail.com>
2024-04-02 17:10:43 +0530

    Symbols Fixed

--- components/Home/About.jsx
+++ components/Home/About.jsx
@@ -8,15 +8,15 @@ const About = () => {
         <h1 className="text-[70px] font-extrabold leading-[5rem]">ABOUT US</h1>
         <p className="text-[22px] text-[#1B3C87]">
           You say you love the planet. <br /> And if you truly do, <br /> you
-          must've felt that guilt while grabbing a plastic <br /> water bottle
-          in your hands... <br /> We know the feeling... <br />
-          And that's why, we, at PAPEL believe that our <br />
-          hydration shouldn'thurt our home. <br /> Grab your eco-friendly
+          must&apos;ve felt that guilt while grabbing a plastic <br /> water
+          bottle in your hands... <br /> We know the feeling... <br />
+          And that&apos;s why, we, at PAPEL believe that our <br />
+          hydration shouldn&apos;t hurt our home. <br /> Grab your eco-friendly
           hydration products to give a <br /> loving hug to nature.
         </p>
         <p className="text-[25px] text-[#1B3C87]">
-          With <strong className="text-[32px] italic">PAPEL</strong>, here's
-          your chance to
+          With <strong className="text-[32px] italic">PAPEL</strong>,
+          here&apos;s your chance to
         </p>
         <h1 className="text-[35px] text-[#1B3C87] font-bold italic">
           Drink The Change
