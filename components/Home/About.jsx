9098805b17024bca4eeaf93de0aadf75ff35e654
Harshit <harshitbhardwaj001@gmail.com>
2024-04-23 15:28:33 +0530

    Minor Fixes

--- components/Home/About.jsx
+++ components/Home/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="relative w-[100vw] h-[110vh] flex gap-[8rem] px-[8rem] pt-[12rem] justify-center xs:max-md:px-[2rem] xs:max-md:pt-0 xs:max-md:flex-col xs:max-md:gap-[1.5rem] xs:max-md:items-center xs:max-md:justify-center xs:max-md:h-[120vh] items-center overflow-x-hidden">
+    <div className="relative w-[100vw] h-[110vh] flex gap-[8rem] px-[8rem] pt-[12rem] justify-center xs:max-md:px-[2rem] xs:max-md:pt-0 xs:max-md:flex-col xs:max-md:gap-[1.5rem] xs:max-md:items-center xs:max-md:justify-center xs:max-md:h-[130vh] items-center overflow-x-hidden">
       <div className="w-[30vw] h-[90vh] rounded-[3rem] shadow-lg bg-[#fff] xs:max-md:hidden overflow-hidden">
         <Image
           src="/9.png"
@@ -19,15 +19,15 @@ const About = () => {
         </h1>
         <p className="text-[22px] text-[#1B3C87] xs:max-md:text-[16px] xs:max-md:text-center font-gilroymedium">
           We started{" "}
-          <strong className="xs:max-md:text-[23px] font-gilroyboldx">
+          <strong className="xs:max-md:text-[23px] font-gilroyboldx xs:max-md:font-gilroybold">
             PAPEL
           </strong>{" "}
           with one single goal: <br className="xs:max-md:hidden" />{" "}
-          <strong className="text-[24px] xs:max-md:text-[23px] font-gilroyboldx ">
+          <strong className="text-[24px] xs:max-md:text-[23px] font-gilroyboldx xs:max-md:font-gilroybold">
             To make India&apos;s most eco-friendly
           </strong>
           <br className="xs:max-md:hidden" />{" "}
-          <strong className="xs:max-md:text-[23px] font-gilroyboldx">
+          <strong className="xs:max-md:text-[23px] font-gilroyboldx xs:max-md:font-gilroybold">
             functional beverage.
           </strong>
           <br className="xs:max-md:hidden" /> Enriched with vitamins & minerals,
@@ -39,14 +39,14 @@ const About = () => {
           <br className="xs:max-md:hidden" />
           which means
           <br className="xs:max-md:hidden" />
-          <strong className="xs:max-md:text-[23px] font-gilroyboldx">
+          <strong className="xs:max-md:text-[23px] font-gilroyboldx xs:max-md:font-gilroybold">
             you can&apos;t see the water, but you&apos;ll feel the change...
           </strong>
           <br className="xs:max-md:hidden" /> That&apos;s how we&apos;ve crafted
           PAPEL for you...
           <br className="xs:max-md:hidden" /> So you get nothing less than
           <br className="xs:max-md:hidden" />
-          <strong className="xs:max-md:text-[23px] font-gilroyboldx">
+          <strong className="xs:max-md:text-[23px] font-gilroyboldx xs:max-md:font-gilroybold">
             a refreshing, crisp sipeach time.
           </strong>
         </p>
@@ -65,7 +65,7 @@ const About = () => {
         alt="about"
         width={400}
         height={500}
-        className="absolute top-[84%] left-[31%] xs:max-md:w-[250px] xs:max-md:left-[51%]"
+        className="absolute top-[84%] left-[31%] xs:max-md:w-[250px] xs:max-md:top-[80%] xs:max-md:left-[51%]"
       />
     </div>
   );
